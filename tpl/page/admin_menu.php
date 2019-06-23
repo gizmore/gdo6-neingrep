8caf4af0733e70602fb86ce4350de2a36fe910c8
gizmore <gizmore@wechall.net>
2019-06-23 02:54:37 +0200

    6.09-r806

--- tpl/page/admin_menu.php
+++ tpl/page/admin_menu.php
@@ -6,5 +6,5 @@ echo GDT_Bar::makeWith(
 	GDT_Link::make('link_admin_section')->href(href('Admin', 'Configure', '&module=NeinGrep')),
 	GDT_Link::make('link_add_user')->href(href('NeinGrep', 'AddUser')),
 	GDT_Link::make('link_add_post')->href(href('NeinGrep', 'AddPost')),
-	GDT_Link::make('link_ranking')->href(href('NeinGrep', 'Ranking')),
+	GDT_Link::make('link_ranking')->href(href('NeinGrep', 'Ranking'))
 )->horizontal()->renderCell();
--- tpl/page/welcome.php
+++ tpl/page/welcome.php
@@ -2,8 +2,23 @@
 use GDO\UI\GDT_Panel;
 
 $html = <<<EOT
-<h1>hi</h1>
+<h1>Hello Dear Internet Client</h1>
+<h2>This is NeinGrep, The 9gag scraper and search engine</h2>
+<h3>Features</h3>
+<ul>
+ <li>Scraping of post metadata</li>
+ <li>Scraping of comments</li>
+ <li>Scraping of username<=>uid relations</li>
+ <li>Revealing hidden OP</li>
+ <li>Ranking of users and posts</li>
+</ul>
+<h3>Planned</h3>
+<ul>
+ <li>Friendmatching based on where they posted or commented</li>
+ <li>Revealing of gender based on comment texts</li>
+</ul>
 EOT;
 
 $box = GDT_Panel::withHTML($html);
 echo $box->render();
+
